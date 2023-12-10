bdecc5d566e2b7622c9a0485d993f7a4bde5eeb5
中村文哉 <fumiya.nakamura0108@gmail.com>
2023-12-10 20:53:15 +0900

    アラーム一覧取得用エンドポイント追加

--- API/__main__.py
+++ API/__main__.py
@@ -1,168 +1,210 @@
 import json
 from flask import Flask, request
-import os
 from modules.edit_json import read_config, write_config
 
 app = Flask(__name__)
 
-dir_path = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.split(dir_path)[0] + "/config.json"
 
-@app.route("/config")
-def hello_world() :
+#---------------
+# アラーム一覧
+#---------------
+@app.route("/alarm")
+def get_alarms() :
 	json_dist = read_config()
-	print(json_dist)
-	return "<p>Hello, World!</p>"
+	alarm_list = {
+		"mon": {
+			"enable": json_dist["mon"]["enable"],
+			"alarm": json_dist["mon"]["alarm"],
+		},
+		"tue": {
+			"enable": json_dist["tue"]["enable"],
+			"alarm": json_dist["tue"]["alarm"],
+		},
+		"wed": {
+			"enable": json_dist["wed"]["enable"],
+			"alarm": json_dist["wed"]["alarm"],
+		},
+		"thu": {
+			"enable": json_dist["thu"]["enable"],
+			"alarm": json_dist["thu"]["alarm"],
+		},
+		"fri": {
+			"enable": json_dist["fri"]["enable"],
+			"alarm": json_dist["fri"]["alarm"],
+		},
+		"sat": {
+			"enable": json_dist["sat"]["enable"],
+			"alarm": json_dist["sat"]["alarm"],
+		},
+		"sun": {
+			"enable": json_dist["sun"]["enable"],
+			"alarm": json_dist["sun"]["alarm"],
+		}
+	}
+	return alarm_list
 
 #---------
 # 月曜日
 #---------
-@app.route("/mon", methods=["GET"])
-def read_mon() :
+@app.route("/<weekday>", methods=["GET"])
+def read_mon(weekday) :
 	# config.jsonの読み込み
 	json_dist = read_config()
-	return json_dist["mon"]
+	return json_dist[weekday]
 
-@app.route("/mon", methods=["POST"])
-def update_mon() :
+@app.route("/<weekday>", methods=["POST"])
+def update_mon(weekday) :
 	# リクエストボディの取得
 	data = request.get_json()
 	# config.jsonの読み込み
 	json_dist = read_config()
-	json_dist["mon"] = data
+	json_dist[weekday] = data
+	json_dist["update"] = True
+	write_config(json_dist)
+	return "UPDATE MONDAY"
+
+@app.route("/alarm/<weekday>", methods=["POST"])
+def update_mon_alarm(weekday) :
+	# リクエストボディの取得
+	data = request.get_json()
+	# config.jsonの読み込み
+	json_dist = read_config()
+	json_dist[weekday]["enable"] = data["enable"]
+	json_dist[weekday]["alarm"] = data["alarm"]
 	json_dist["update"] = True
 	write_config(json_dist)
 	return "UPDATE MONDAY"
 
 
-#---------
-# 火曜日
-#---------
-@app.route("/tue")
-def read_tue() :
-	# config.jsonの読み込み
-	json_dist = read_config()
-	return json_dist["tue"]
 
-@app.route("/tue", methods=["POST"])
-def update_tue() :
-	# リクエストボディの取得
-	data = request.get_json()
-	# config.jsonの読み込み
-	json_dist = read_config()
-	json_dist["tue"] = data
-	json_dist["update"] = True
-	write_config(json_dist)
-	return "<p>UPDATE TUESDAY</p>"
+# #---------
+# # 火曜日
+# #---------
+# @app.route("/tue")
+# def read_tue() :
+# 	# config.jsonの読み込み
+# 	json_dist = read_config()
+# 	return json_dist["tue"]
 
-
-#---------
-# 水曜日
-#---------
-@app.route("/wed")
-def read_wed() :
-	# config.jsonの読み込み
-	json_dist = read_config()
-	return json_dist["wed"]
-
-@app.route("/wed", methods=["POST"])
-def update_wed() :
-	# リクエストボディの取得
-	data = request.get_json()
-	# config.jsonの読み込み
-	json_dist = read_config()
-	json_dist["wed"] = data
-	json_dist["update"] = True
-	write_config(json_dist)
-	return "<p>UPDATE WEDNESDAY</p>"
+# @app.route("/tue", methods=["POST"])
+# def update_tue() :
+# 	# リクエストボディの取得
+# 	data = request.get_json()
+# 	# config.jsonの読み込み
+# 	json_dist = read_config()
+# 	json_dist["tue"] = data
+# 	json_dist["update"] = True
+# 	write_config(json_dist)
+# 	return "<p>UPDATE TUESDAY</p>"
 
 
-#---------
-# 木曜日
-#---------
-@app.route("/thu")
-def read_thu() :
-	# config.jsonの読み込み
-	json_dist = read_config()
-	return json_dist["thu"]
+# #---------
+# # 水曜日
+# #---------
+# @app.route("/wed")
+# def read_wed() :
+# 	# config.jsonの読み込み
+# 	json_dist = read_config()
+# 	return json_dist["wed"]
 
-@app.route("/thu", methods=["POST"])
-def update_thu() :
-	# リクエストボディの取得
-	data = request.get_json()
-	# config.jsonの読み込み
-	json_dist = read_config()
-	json_dist["thu"] = data
-	json_dist["update"] = True
-	write_config(json_dist)
-	return "<p>UPDATE THURSDAY</p>"
-
-
-#---------
-# 金曜日
-#---------
-@app.route("/fri")
-def read_fri() :
-	# config.jsonの読み込み
-	json_dist = read_config()
-	return json_dist["fri"]
-
-@app.route("/fri", methods=["POST"])
-def update_fri() :
-	# リクエストボディの取得
-	data = request.get_json()
-	# config.jsonの読み込み
-	json_dist = read_config()
-	json_dist["fri"] = data
-	json_dist["update"] = True
-	write_config(json_dist)
-	return "<p>UPDATE FRIDAY</p>"
+# @app.route("/wed", methods=["POST"])
+# def update_wed() :
+# 	# リクエストボディの取得
+# 	data = request.get_json()
+# 	# config.jsonの読み込み
+# 	json_dist = read_config()
+# 	json_dist["wed"] = data
+# 	json_dist["update"] = True
+# 	write_config(json_dist)
+# 	return "<p>UPDATE WEDNESDAY</p>"
 
 
-#---------
-# 土曜日
-#---------
-@app.route("/sat")
-def read_sat() :
-	# config.jsonの読み込み
-	json_dist = read_config()
-	return json_dist["sat"]
+# #---------
+# # 木曜日
+# #---------
+# @app.route("/thu")
+# def read_thu() :
+# 	# config.jsonの読み込み
+# 	json_dist = read_config()
+# 	return json_dist["thu"]
 
-@app.route("/sat", methods=["POST"])
-def update_sat() :
-	# リクエストボディの取得
-	data = request.get_json()
-	# config.jsonの読み込み
-	json_dist = read_config()
-	json_dist["sat"] = data
-	json_dist["update"] = True
-	write_config(json_dist)
-	return "<p>UPDATE SATADAY</p>"
+# @app.route("/thu", methods=["POST"])
+# def update_thu() :
+# 	# リクエストボディの取得
+# 	data = request.get_json()
+# 	# config.jsonの読み込み
+# 	json_dist = read_config()
+# 	json_dist["thu"] = data
+# 	json_dist["update"] = True
+# 	write_config(json_dist)
+# 	return "<p>UPDATE THURSDAY</p>"
 
 
-#---------
-# 日曜日
-#---------
-@app.route("/sun")
-def read_sun() :
-	# config.jsonの読み込み
-	json_dist = read_config()
-	return json_dist["sun"]
+# #---------
+# # 金曜日
+# #---------
+# @app.route("/fri")
+# def read_fri() :
+# 	# config.jsonの読み込み
+# 	json_dist = read_config()
+# 	return json_dist["fri"]
 
-@app.route("/sun", methods=["POST"])
-def update_sun() :
-	# リクエストボディの取得
-	data = request.get_json()
-	# config.jsonの読み込み
-	json_dist = read_config()
-	json_dist["sun"] = data
-	json_dist["update"] = True
-	write_config(json_dist)
-	return "<p>UPDATE WEDNESDAY</p>"
+# @app.route("/fri", methods=["POST"])
+# def update_fri() :
+# 	# リクエストボディの取得
+# 	data = request.get_json()
+# 	# config.jsonの読み込み
+# 	json_dist = read_config()
+# 	json_dist["fri"] = data
+# 	json_dist["update"] = True
+# 	write_config(json_dist)
+# 	return "<p>UPDATE FRIDAY</p>"
+
+
+# #---------
+# # 土曜日
+# #---------
+# @app.route("/sat")
+# def read_sat() :
+# 	# config.jsonの読み込み
+# 	json_dist = read_config()
+# 	return json_dist["sat"]
+
+# @app.route("/sat", methods=["POST"])
+# def update_sat() :
+# 	# リクエストボディの取得
+# 	data = request.get_json()
+# 	# config.jsonの読み込み
+# 	json_dist = read_config()
+# 	json_dist["sat"] = data
+# 	json_dist["update"] = True
+# 	write_config(json_dist)
+# 	return "<p>UPDATE SATADAY</p>"
+
+
+# #---------
+# # 日曜日
+# #---------
+# @app.route("/sun")
+# def read_sun() :
+# 	# config.jsonの読み込み
+# 	json_dist = read_config()
+# 	return json_dist["sun"]
+
+# @app.route("/sun", methods=["POST"])
+# def update_sun() :
+# 	# リクエストボディの取得
+# 	data = request.get_json()
+# 	# config.jsonの読み込み
+# 	json_dist = read_config()
+# 	json_dist["sun"] = data
+# 	json_dist["update"] = True
+# 	write_config(json_dist)
+# 	return "<p>UPDATE WEDNESDAY</p>"
 
 
 if __name__ == "__main__" :
-	app.run(port=8000)
+	app.run(port=8000, debug=True)
 
 """
 curl -X POST -H "Content-Type: application/json" -d '{
@@ -191,8 +233,9 @@ curl -X POST -H "Content-Type: application/json" -d '{
       "answer": 10
     }
   }
-}' localhost:8000/tue
+}' localhost:8000/alarm/mon
 
+curl -X POST -H "Content-Type: application/json" -d '{"enable": true, "alarm": "1330" }' localhost:8000/alarm/mon
 
 curl localhost:8000/tue
 """
